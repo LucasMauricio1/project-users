c0c6ea7d70a06543438e99219c127c410a8a63b2
lucasmauricio1 <lucasmauriciofc@gmail.com>
2024-04-11 11:33:41 -0300

    feat: create database connection

--- api-nestjs/src/app.module.ts
+++ api-nestjs/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from './app/user/user.module'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      port: Number(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
